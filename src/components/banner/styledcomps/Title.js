4adef90cc1c451fec74fc7fa570903a31f7e4818
Christian <ChrisWcs@users.noreply.github.com>
2017-12-04 13:21:49 -0700

    Add some responsiveness to Banner

--- src/components/banner/styledcomps/Title.js
+++ src/components/banner/styledcomps/Title.js
@@ -5,6 +5,10 @@ const Title = styled.h1`
     font-size: 60px;
     letter-spacing: 2px;
     color: white;
+
+    @media (max-device-width: 480px) {
+        font-size: 30px;
+    }
 `;
 
 export default Title;
