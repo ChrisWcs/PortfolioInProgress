7ba2c023d236bf16679b944a6d35889434269657
Christian <ChrisWcs@users.noreply.github.com>
2018-01-13 11:17:23 -0700

    Added state to Lang component which will expand to show Technology

--- src/components/langexperience/Lang.js
+++ src/components/langexperience/Lang.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactShow from 'react-show';
 
 class Lang extends React.Component {
+    constructor(props){
+        super(props);
 
+        this.state = {
+            isExpanded: false
+        };
+
+        this.expand = this.expand.bind(this);
+    }
+
+    expand(){
+        this.setState( prevState => ({
+            isExpanded: !prevState.isExpanded
+        }));
+    }
 
     render(){
         return (
             <div>
-                <ReactShow show={true}/>
+                <ReactShow show={this.state.isExpanded}/>
             </div>
         );
     }
